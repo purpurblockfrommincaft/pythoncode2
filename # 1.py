9ff7d2dbdd87fa26cddedc702d1d8e3c3c792b6f
ultimatepurpurblock <saisankarpranav@gmail.com>
2025-01-17 19:08:38 +0530

    Add files via upload

---
+++ # 1.py
@@ -0,0 +1,31 @@
+# 1.A program that calculates the square of a number:
+# 2.A program to calculate the area of a rectangle:
+# 3.reverse a given string
+
+
+
+
+
+
+
+# 2
+
+# def area(a,b):
+#     print(a*b)
+
+# area(2,4)
+
+
+
+# 3
+# name="sonic"
+# def rev():
+#     print(name[::-1])
+
+# rev()
+
+
+
+
+f=open("sega.txt","r")
+print(f.read())
